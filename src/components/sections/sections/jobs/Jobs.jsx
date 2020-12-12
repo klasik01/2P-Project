e10e15fe34f7d829c30a0ebe132d12305101fba0
Stanislav Kasika <stanislav.kasika@gmail.com>
2020-12-12 21:51:40 +0100

    update job section

--- src/components/sections/sections/jobs/Jobs.jsx
+++ src/components/sections/sections/jobs/Jobs.jsx
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
 import {ArticleLayout} from '../../../common/Layout';
-import {Col, Row} from 'reactstrap';
+import {Col} from 'reactstrap';
 import jobsData from '../../../../data/jobs.json';
 import textsData from '../../../../data/texts.json';
-import {Hr} from "../../../../theme/globalStyle";
-import {PRIMARY} from "../../../../theme/colors";
+import {Hr} from '../../../../theme/globalStyle';
+import {PRIMARY} from '../../../../theme/colors';
+import workerWorkerImg from '../../../../images/worker-svg.svg';
+import workerMisterImg from '../../../../images/worker-mister.svg';
 
 const Jobs = () => {
 
@@ -14,20 +16,39 @@ const Jobs = () => {
     return (
         <ArticleLayout header={texts.title} footer="">
             <h2 className="py-3 font-weight-bold">{texts.par[0]}</h2>
-            <Row>
-                {data.map(item =>
-                    <Col md={5} lg={4} key={item.id} className="m-1 card card-body bg-light">
-                        <h3 style={{color: PRIMARY}} className="font-weight-bold text-center">{item.contract}</h3>
-                        <Hr />
-                        {item.sections.map(data => <div key={data.id}>
-                            <h4 style={{color: PRIMARY}}>{data.title}</h4>
-                            <ul>
-                                {data.list.map((listItem, index) => <li key={data.id + '-' + index}>{listItem}</li>)}
-                            </ul>
-                        </div>)}
-                    </Col>
-                )}
-            </Row>
+            <div className="container-fluid">
+                <div className="d-flex flex-wrap align-self-stretch">
+                    {data.map(item =>
+                        <div className="d-flex p-2 align-self-stretch">
+                            <div className="card user-card">
+                                <div className="card-block">
+                                    <div className="card-body">
+                                        <div className="p-3 text-center">
+                                            <h3 style={{color: PRIMARY}}
+                                                className="font-weight-bold text-center">{item.contract}</h3>
+                                            <Col className="offset-2 col-8">
+                                                <img
+                                                    className="img-radius"
+                                                    alt={item.contract}
+                                                    src={item.img === 'workerWorkerImg' ? workerWorkerImg : item.img === 'workerMisterImg' ? workerMisterImg : ''}
+                                                />
+                                            </Col>
+                                        </div>
+                                        <Hr/>
+                                        {item.sections.map(data => <div key={data.id}>
+                                            <h4 style={{color: PRIMARY}}>{data.title}</h4>
+                                            <ul>
+                                                {data.list.map((listItem, index) => <li
+                                                    key={data.id + '-' + index}>{listItem}</li>)}
+                                            </ul>
+                                        </div>)}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
         </ArticleLayout>
     );
 };
