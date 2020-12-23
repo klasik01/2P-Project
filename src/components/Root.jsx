66729db1efb978a949d74bfcacad71be9632abf0
Stanislav Kasika <stanislav.kasika-extern@renault.cz>
2020-12-23 13:00:14 +0100

    add google analytics

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -3,6 +3,11 @@ import {Router} from 'react-router-dom';
 import App from './App';
 import history from '../history';
 import SimpleReactLightbox from 'simple-react-lightbox';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-186010871-1');
+// eslint-disable-next-line no-undef
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const Root = () => (
     <SimpleReactLightbox>
